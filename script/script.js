db9a8f8dd62dfa0ca98eb80ab13fbf240897e418
Bassetheboss <143607666+Bassetheboss@users.noreply.github.com>
2023-09-14 09:40:06 +0200

    ny

---
+++ script/script.js
@@ -0,0 +1,6 @@
+hamburger = document.querySelector(".hamburger");
+navBar = document.querySelector(".nav-bar");
+hamburger.onclick = function(){
+    alert("yes")
+    navBar.classlist.toggel("active");
+}
